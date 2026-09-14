4eb0317c501f4834250918de513a8847748c1ef2
ToasterTony <billyhein06@gmail.com>
2026-09-13 20:21:36 -0500

    Add files via upload

---
+++ Assets/Scenes/TriangleScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SquareInput example = SquareInput.GetComponent<SquareInput>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/SquareInput.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class SquareInput : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public bool Health;
+    public int Coins;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void onMove()
+    {
+       Debug.Log("Input!");
+    }
+}
